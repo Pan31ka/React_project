55228e253b7a8f05654fcd59daece25cda228624
Pan31ka <xoxlov789@gmail.com>
2021-05-07 22:35:28 +0300

    NavLine and other

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 const Navbar = () => {
-    return <nav className={classes.nav}>
-        <div className={classes.item}>
-            <a href="/profile">Profile</a>
-        </div>
-        <div className={`${classes.item} ${classes.active}`}>
-            <a href="/dialogs">Messages</a>
-        </div>
-        <div className={classes.item}>
-            <a href="/news">News</a>
-        </div>
-        <div className={classes.item}>
-            <a href="/music">Music</a>
-        </div>
-        <div className={classes.item}>
-            <a href="/setting">Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={classes.nav}>
+            <div className={classes.item}>
+                <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
+            </div>
+            <div className={`${classes.item} ${classes.active}`}>
+                <NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/news" activeClassName={classes.active}>News</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/music" activeClassName={classes.active}>Music</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/setting" activeClassName={classes.active}>Settings</NavLink>
+            </div>
+        </nav>)
 }
 
 export default Navbar;
--- src/components/navbar/sideBar_component/Dialogs/dialogs.jsx
+++ src/components/navbar/sideBar_component/Dialogs/dialogs.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import classes from './Dialogs.module.css';
 import s from './Dialogs.module.css';
 const Dialogs = (props) => {
     return <div className={s.item}>
-            dialog
+        <div className={s.dialogs}>
+            <div className={s.dialog}>
+                HE
+            </div>
+            <div className={s.dialog+' '+s.active}>
+                HE
+            </div>
+            <div className={s.dialog}>
+                HE
+            </div>
+            <div className={s.dialog}>
+                HE
+            </div>
+            <div className={s.dialog}>
+                HE
+            </div>
+        </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Are you useless?
+                </div>
+                <div className={s.message}>
+                    Are you useless?
+                </div>
+                <div className={s.message}>
+                    Are you useless?
+                </div>
+                <div className={s.message}>
+                    Are you useless?
+                </div>
+                <div className={s.message}>
+                    Are you useless?
+                </div>
+            </div>
+    
 
 
     </div>
