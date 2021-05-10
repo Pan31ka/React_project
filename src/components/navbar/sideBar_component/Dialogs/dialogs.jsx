ec059bb1e662afa70fbe1ad695f6230fb0559b59
Pan31ka <xoxlov789@gmil.com>
2021-05-10 20:56:22 +0300

    .MAP and other (23-25)

--- src/components/navbar/sideBar_component/Dialogs/dialogs.jsx
+++ src/components/navbar/sideBar_component/Dialogs/dialogs.jsx
@@ -3,38 +3,49 @@ import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
 import s from './Dialogs.module.css';
 const DialogItem = (props) => {
-    return (
-        <div className={s.dialog}>
-            <NavLink to={"/dialogs/" + props.id} activeClassName={classes.active}> {props.name}</NavLink>
-        </div>
-    )
+	return (
+		<div className={s.dialog}>
+			<NavLink to={"/dialogs/" + props.id} activeClassName={classes.active}> {props.name}</NavLink>
+		</div>
+	)
 }
 const MessageItem = (props) => {
-    return (
-        <div className={s.message}>
-            {props.text}
-        </div>
-    )
+	return (
+		<div className={s.message}>
+			{props.text}
+		</div>
+	)
 }
 const Dialogs = (props) => {
-    return (
-        <div className={s.item}>
-            <div className={s.dialogs}>
-                <DialogItem name="Nikita" id="1" />
-                <DialogItem name="Nik" id="2" />
-                <DialogItem name="Nikita" id="3" />
-                <DialogItem name="Nikita" id="4" />
-                <DialogItem name="Nikita" id="5" />
+	let DialogsData = [
+		{ id: 1, name: 'Nikita' },
+		{ id: 2, name: 'Nikita' },
+		{ id: 3, name: 'Nikita' },
+		{ id: 4, name: 'Nikita' },
+		{ id: 5, name: 'Nikita' },
+		{ id: 6, name: 'Nikita' }
+	]
 
-            </div>
-            <div className={s.messages}>
-                <MessageItem text=" Are you useless?" />
-                <MessageItem text=" Are you useless?" />
-                <MessageItem text=" Are you useless?" />
-                <MessageItem text=" Are you useless?" />
-                <MessageItem text=" Are you useless?" />
-            </div>
-        </div>
-    )
+	let MessagesData = [
+		{ id: 1, text: ' Are you useless?' },
+		{ id: 2, text: ' Are you useless?' },
+		{ id: 3, text: ' Are you useless?' },
+		{ id: 4, text: ' Are you useless?' },
+		{ id: 5, text: ' Are you useless?' },
+		{ id: 6, text: ' Are you useless?' },
+	]
+	let DialogsElement = DialogsData.map(d => <DialogItem name={d.name} id={d.id} />)
+	let MessagesElement = MessagesData.map(m => <MessageItem text={m.text} />)
+	return (
+		<div className={s.item}>
+			<div className={s.dialogs}>
+				{DialogsElement}
+
+			</div>
+			<div className={s.messages}>
+				{MessagesElement}
+			</div>
+		</div>
+	)
 }
 export default Dialogs;
--- src/components/navbar/sideBar_component/Profile/myposts/MyPost.jsx
+++ src/components/navbar/sideBar_component/Profile/myposts/MyPost.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import s1 from  './MyPost.module.css'
+import s1 from './MyPost.module.css'
 import Post from './post/post';
 const MyPost = () => {
-    return <div className={s1.item}>
-      <h2>My posts</h2>
-      <div >
-        <h4>New post</h4>
-      </div>
-      <div>
-        <textarea className={s1.border}> </textarea>
-      </div>
-      <div >
-        <button >Post this</button>
-      </div>
-      <div >
-       <Post message='hi, how are you' like=' 15'/>
-       <Post message='it my first project'like=' 351'/>
-       <Post/>
-       <Post/>
-       <Post/>
-      </div>
-    </div>
- 
+	let MyPostData = [
+		{ id: 1, text: 'hi, how are you', like: '15' },
+		{ id: 2, text: 'it my first project', like: '351' }
+	]
+	let MyPostEl = MyPostData.map(p => <Post message={p.text} like={p.like} />)
+	return <div className={s1.item}>
+		<h2>My posts</h2>
+		<div >
+			<h4>New post</h4>
+		</div>
+		<div>
+			<textarea className={s1.border}> </textarea>
+		</div>
+		<div >
+			<button >Post this</button>
+		</div>
+		<div >
+			{MyPostEl}
+
+		</div>
+	</div>
+
 }
 
 export default MyPost;
