d523b202a58d143acaddbf0cf6862e5c8d2922b1
Pan31ka <xoxlov789@gmil.com>
2021-05-17 22:12:11 +0300

    add onClick and ref2

--- src/components/navbar/sideBar_component/Dialogs/Message/MessageItem.jsx
+++ src/components/navbar/sideBar_component/Dialogs/Message/MessageItem.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import s from './MessageItem.module.css';
 
 const MessageItem = (props) => {
+	let newMessageElement = React.createRef();
+	let addMessages = () => {
+		let text = newMessageElement.current.value;
+		alert(text);
+	}
 	return (
 		<div className={s.message}>
 			{props.text}
+			<textarea ref={newMessageElement}></textarea>
+			<button onClick={addMessages}>Send</button>
 		</div>
 	)
 }
--- src/components/navbar/sideBar_component/Profile/myposts/MyPost.jsx
+++ src/components/navbar/sideBar_component/Profile/myposts/MyPost.jsx
@@ -4,16 +4,21 @@ import Post from './post/post';
 const MyPost = (props) => {
 
 	let MyPostEl = props.MyPostData.map(p => <Post message={p.text} like={p.like} />)
+	let newPostElement = React.createRef();
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		alert(text);
+	}
 	return <div className={s1.item}>
 		<h2>My posts</h2>
 		<div >
 			<h4>New post</h4>
 		</div>
 		<div>
-			<textarea className={s1.border}> </textarea>
+			<textarea className={s1.border} ref={newPostElement}> </textarea>
 		</div>
 		<div >
-			<button >Post this shit</button>
+			<button onClick={addPost}>Post this shit</button>
 		</div>
 		<div >
 			{MyPostEl}
